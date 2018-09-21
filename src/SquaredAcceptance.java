bd7db03b691110782b4aaa0dd022388166b3d80d
jzoon <j.zoon@student.tudelft.nl>
2018-09-21 11:27:42 +0200

    Added comments for clarity

--- src/SquaredAcceptance.java
+++ src/SquaredAcceptance.java
@@ -37,36 +37,50 @@ public class SquaredAcceptance extends AcceptanceStrategy {
 
     @Override
     public Actions determineAcceptability() {
+    	// Get my first bid utility if I already offered a bid
     	double myFirstBid = 1;
     	if (negotiationSession.getTimeline().getCurrentTime() == 0) {
     		myFirstBid = negotiationSession.getOwnBidHistory().getFirstBidDetails().getMyUndiscountedUtil();
     	} 
-    	
+    
+    	// Get opponents best bid utility
         double opponentsBestBid = negotiationSession.getOpponentBidHistory().getBestBidDetails()
                 .getMyUndiscountedUtil();
+        
+        // Get my next bid utility
         double nextMyBidUtil = offeringStrategy.getNextBid().getMyUndiscountedUtil();
+        
+        // Get opponents last bid utility
         double lastOpponentBidUtil = negotiationSession.getOpponentBidHistory().getLastBidDetails()
                 .getMyUndiscountedUtil();
 
+        // Percentage time left, number between 1 and 0.
         double percentageTimeLeft = (negotiationSession.getTimeline().getTotalTime() -
                 negotiationSession.getTimeline().getCurrentTime())/negotiationSession.getTimeline().getTotalTime();
 
+        // Minimum offer is the opponents best bid, or my opening bid divided by variable alpha. (which one is highest)
         double minimumOffer = 1;
-        if (myFirstBid/1.1 > opponentsBestBid) {
-            minimumOffer = myFirstBid/1.1;
+        if (myFirstBid/1.2 > opponentsBestBid) {
+            minimumOffer = myFirstBid/1.2;
         } else {
             minimumOffer = opponentsBestBid;
         }
 
+        // The difference in my utility between my first bid and the opponent best bid.
         double startingDifference = myFirstBid - opponentsBestBid;
         
+        // An acceptable offer: the closer we come to the end of a negotiation, the lower it gets. In the last round it is
+        // equal to the minimumoffer variable.
         double acceptableOffer = Math.sqrt(percentageTimeLeft*startingDifference) + minimumOffer;
 
+        // Accept an offer if it is better than my next bid OR if it is better than the acceptableOffer variable
         if (lastOpponentBidUtil >= nextMyBidUtil && lastOpponentBidUtil >= opponentsBestBid) {
             return Actions.Accept;
         } else if (lastOpponentBidUtil >= acceptableOffer) {
             return Actions.Accept;
         }
+        
+        // else: reject
         return Actions.Reject;
     }
 
